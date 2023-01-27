d5d79931742bc53a838954fa1b23cdb664934a69
ASHISH YADAV <ashish.yd@gmail.com>
2023-01-27 14:18:05 +0530

    react todo list
    simple react todo list built using phoenix

---
+++ react_todo_list/lib/react_todo_list_web/controllers/page_controller.ex
@@ -0,0 +1,8 @@
+defmodule ReactTodoListWeb.PageController do
+  use ReactTodoListWeb, :controller
+  alias ReactTodoList.Todo
+  def index(conn, _params) do
+      tasks = Todo.list_tasks()
+      render(conn, "index.html", props: Poison.encode!(%{tasks: tasks}))
+  end
+end
---
+++ react_todo_list/priv/repo/seeds.exs
@@ -0,0 +1,23 @@
+# Script for populating the database. You can run it as:
+#
+#     mix run priv/repo/seeds.exs
+#
+# Inside the script, you can read and write to any of your
+# repositories directly:
+#
+#     ReactTodoList.Repo.insert!(%ReactTodoList.SomeSchema{})
+#
+# We recommend using the bang functions (`insert!`, `update!`
+# and so on) as they will fail if something goes wrong.
+alias ReactTodoList.Repo
+alias ReactTodoList.Todo.Task
+Repo.insert!(%Task{
+  id: 1,
+  description: "Learn Elixir",
+  completed: false
+})
+Repo.insert!(%Task{
+  id: 2,
+  description: "Build a Todo App",
+  completed: false
+})
